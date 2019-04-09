9e803ccc316c664b75b795c772bd019ca69170bb
Jim LeBeau <jrlebeau@yahoo.com>
2019-04-09 13:14:02 -0700

    POST with Headers

--- src/main/java/com/jrl/employeetracker/rest/exception/CustomExceptionHandler.java
+++ src/main/java/com/jrl/employeetracker/rest/exception/CustomExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @SuppressWarnings({"unchecked", "rawtypes"})
 @ControllerAdvice
